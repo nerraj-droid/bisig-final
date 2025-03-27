096d72d1644132c8685efdb25b605574f8793ff1
nerraj-droid <68629963+nerraj-droid@users.noreply.github.com>
2025-03-27 14:02:38 +0800

    disabled linting rules

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'placehold.co'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig 
